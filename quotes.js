8e4b5fb165cc358b3f08e4a04ed4e9c4f50d7083
SimonB <58704301+SimonGoBrrr@users.noreply.github.com>
2022-05-16 16:48:50 +0200

    more quotes

--- quotes.js
+++ quotes.js
@@ -4,7 +4,13 @@ let quotes = [
 "``Give a man a truth, and he will think for a day. Give a man reason, and he will think for a lifetime.´´",
 "``Nobody exists on purpose. Nobody belongs anywhere. We're all going to die. Come watch TV.´´",
 "``What, so everyone's supposed to sleep every single night now?´´",
-"``Loneliness is often the byproduct of a gifted mind.´´"
+"``Loneliness is often the byproduct of a gifted mind.´´",
+"``If you reveal your secrets to the wind, you should not blame the wind for revealing them to the trees.´´",
+"``If privacy is outlawed, only outlaws will have privacy.´´",
+"``Is evil something you are? Or is it something you do?´´",
+"``Study hard what interests you the most in the most undisciplined, irreverent and original manner possible.´´",
+"``Physics is like sex: sure, it may give some practical results, but that's not why we do it.´´",
+"`I learned very early the difference between knowing the name of something and knowing something.´´"
 ]
 
 function getRandomIntInclusive(min, max) {
