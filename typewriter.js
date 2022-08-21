186b86e9ad15a32e9b1c7119b003207b83a7b277
SimonB <simonthedankone@gmail.com>
2022-08-21 04:08:30 +0200

    new page

--- typewriter.js
+++
@@ -1,9 +0,0 @@
-const myName = "Simon Brown."
-let interval = 0;
-let startText = "";
-setInterval(() => {
-    if(startText.length == myName.length) return;
-    startText+=myName[interval];
-    document.getElementById("name").textContent=startText;
-    interval++;
-}, 175);
