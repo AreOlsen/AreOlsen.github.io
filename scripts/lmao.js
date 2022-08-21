5ace86c0320c664975bdbe7fcbbcd0ff68adcd6f
SimonB <simonthedankone@gmail.com>
2022-08-21 14:01:35 +0200

    idunno

---
+++ scripts/lmao.js
@@ -0,0 +1,4 @@
+console.log(
+	"If you are reading this then you probably understand how shit this site is built.\n" +
+		"So Iunno what to tell ya man, just accept it? I kind of built this without taking any precautions or planning."
+);
