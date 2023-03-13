0eb8e73dc780f32541b8829e3b59d28496789630
SimonB <simonthedankone@gmail.com>
2023-03-13 09:25:18 +0100

    Redirecting to new website.

--- scripts/lmao.js
+++
@@ -1,4 +0,0 @@
-console.log(
-	"If you are reading this then you probably understand how shit this site is built.\n" +
-		"So Iunno what to tell ya man, just accept it? I kind of built this without taking any precautions or planning."
-);
